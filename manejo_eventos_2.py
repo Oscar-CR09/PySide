6a592f694dbfee74169cf2e0d113f760780a9b8b
oscar cervantes <oscarcr189@outlook.com>
2022-12-25 17:18:46 -0600

    modificacion2

--- manejo_eventos_2.py
+++ manejo_eventos_2.py
@@ -11,23 +11,10 @@ class VentanaPrincipal(QMainWindow):
         # Boton
         boton = QPushButton('Click aqui')
         # conectamos el evento checado(por defaul es false)
-        boton.setCheckable(True)
-        # conectamos otro slot al evento checar
-        boton.clicked.connect(self.event_chechar)
-        # conectamos el evento signal click con el slot (evento_click)
-        boton.clicked.connect(self.evento_click)
 
         # Publicamos al boton
         self.setCentralWidget(boton)
 
-    def event_chechar(self, checar):
-        self.boton_checado = checar
-        print('Checado', self.boton_checado)
-
-    def evento_click(self):
-        print('Has hecho click')
-        # accedemos al boton checado
-        print('boton checado desde evento click', self.boton_checado)
 
 
 if __name__ == '__main__':
