2639bcff1053c7d7d5a1c3d52fcfce0cc21a0191
oscar cervantes <oscarcr189@outlook.com>
2022-12-25 21:00:41 -0600

    click

--- manejo_eventos_2.py
+++ manejo_eventos_2.py
@@ -9,12 +9,23 @@ class VentanaPrincipal(QMainWindow):
         super().__init__()
         self.setWindowTitle('Signal y Slots')
         # Boton
-        boton = QPushButton('Click aqui')
-        # conectamos el evento checado(por defaul es false)
-
+        self.boton = QPushButton('Click aqui')
+        # Asociamos la señal de click al slot evento click
+        self.boton.clicked.connect(self.evento_click)
+        #conecatar a la señal de cambio de titulo 
+        self.windowTitleChanged.connect(self.cambio_titulo_aplicacion)
         # Publicamos al boton
-        self.setCentralWidget(boton)
+        self.setCentralWidget(self.boton)
 
+    def evento_click(self):
+        #cambiar el textodel boton y el titulo de la ventana 
+        self.boton.setText('Nuevo texto boton')
+        self.boton.setEnabled(False)
+        self.setWindowTitle('Nuevo titulo de la aplicacion ')
+        print('Evento click')
+
+    def cambio_titulo_aplicacion(self,nuevotitulo):
+        print(f'NUevotitulo aplicacion: {nuevotitulo}')
 
 
 if __name__ == '__main__':
