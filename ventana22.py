d530a1f3fa5a738965038273fc802d579cb3b179
oscar cervantes <oscarcr189@outlook.com>
2023-01-05 22:59:51 -0600

    label y se configura al mismo tiempo

--- ventana22.py
+++ ventana22.py
@@ -1,13 +1,13 @@
 from random import randint
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QPalette, QColor, QAction, QIcon
-from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout, QStackedLayout, QPushButton, QTabWidget, QLabel, QToolBar, QStatusBar, QCheckBox, QDialog, QDialogButtonBox, QMessageBox
+from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout, QStackedLayout, QPushButton, QTabWidget, QLabel, QToolBar, QStatusBar, QCheckBox, QDialog, QDialogButtonBox, QMessageBox,QLineEdit
 
 
 class NuevaVentana(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Nueva ventana')
+        self.setWindowTitle('Python')
         layout = QVBoxLayout()
         self.etiqueta = QLabel(f'Nuevo Ventana: {randint(0,100)}')
         layout.addWidget(self.etiqueta)
@@ -21,7 +21,16 @@ class VentanaPrincipal(QMainWindow):
         self.setWindowTitle('Ventana')
         self.boton = QPushButton('Mostrar/ocultar nueva ventana')
         self.boton.clicked.connect(self.mostrar_nueva_ventana)
-        self.setCentralWidget(self.boton)
+        #definimos una entrada de texto
+        self.entrada_texto = QLineEdit()
+        self.entrada_texto.textChanged.connect(self.nueva_ventan.etiqueta.setText)
+        #layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.boton)
+        layout.addWidget(self.entrada_texto)
+        contenedor = QWidget()
+        contenedor.setLayout(layout)
+        self.setCentralWidget(contenedor)
 
     def mostrar_nueva_ventana(self, estado):
         if self.nueva_ventan.isVisible():
